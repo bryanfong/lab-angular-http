7716648a0b198d450a244e5e4de403db600a3a4e
Bryan Fong <bryan.fch@gmail.com>
2015-12-02 08:53:34 +0800

    get and add criminals are done

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,1 +1,30 @@
-console.log("test")
+angular.module('criminalApp', [])
+.controller('criminalCtrl', ['$scope', '$http', function ($scope, $http){
+
+  $scope.criminals = [];
+  $scope.newCriminal = {};
+  $scope.addCriminal = addCriminal;
+  $scope.updateCriminal = updateCriminal;
+
+  getCriminals();
+
+  function getCriminals(){
+    $http
+      .get('http://localhost:3000/criminals')
+      .success(function(data){
+        $scope.criminals = data.criminals;
+      });
+  }
+
+  function addCriminal(){
+    $http
+      .post('http://localhost:3000/criminals', $scope.newCriminal)
+      .success(function(){
+        getCriminals();
+      });
+    $scope.newCriminal ={}
+  }
+
+
+
+}])
